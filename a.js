2f528a172517555bfa8463d4858484d601cedd40
Orton1269 <ikundu66@gmail.com>
2021-05-29 01:26:26 +0530

    done

---
+++ a.js
@@ -0,0 +1,131 @@
+function bell()
+{
+  var audio = new Audio('a.mp3');
+  audio.play();
+}
+function bellb()
+{
+  var audio = new Audio('b.mp3');
+  audio.play();
+}
+function bellc()
+{
+  var audio = new Audio('c.mp3');
+  audio.play();
+}
+function belld()
+{
+  var audio = new Audio('d.mp3');
+  audio.play();
+}
+function belle()
+{
+  var audio = new Audio('e.mp3');
+  audio.play();
+}
+function bellf()
+{
+  var audio = new Audio('f.mp3');
+  audio.play();
+}
+function bellg()
+{
+  var audio = new Audio('g.mp3');
+  audio.play();
+}
+function bellh()
+{
+  var audio = new Audio('h.mp3');
+  audio.play();
+}
+function belli()
+{
+  var audio = new Audio('i.mp3');
+  audio.play();
+}
+function bellj()
+{
+  var audio = new Audio('j.mp3');
+  audio.play();
+}
+function bellk()
+{
+  var audio = new Audio('k.mp3');
+  audio.play();
+}
+function belll()
+{
+  var audio = new Audio('l.mp3');
+  audio.play();
+}
+function bellm()
+{
+  var audio = new Audio('m.mp3');
+  audio.play();
+}
+function belln()
+{
+  var audio = new Audio('n.mp3');
+  audio.play();
+}
+function bello()
+{
+  var audio = new Audio('o.mp3');
+  audio.play();
+}
+function bellp()
+{
+  var audio = new Audio('p.mp3');
+  audio.play();
+}
+function bellq()
+{
+  var audio = new Audio('q.mp3');
+  audio.play();
+}
+function bellr()
+{
+  var audio = new Audio('r.mp3');
+  audio.play();
+}
+function bells()
+{
+  var audio = new Audio('s.mp3');
+  audio.play();
+}
+function bellt()
+{
+  var audio = new Audio('t.mp3');
+  audio.play();
+}
+function bellu()
+{
+  var audio = new Audio('u.mp3');
+  audio.play();
+}
+function bellv()
+{
+  var audio = new Audio('v.mp3');
+  audio.play();
+}
+function bellw()
+{
+  var audio = new Audio('w.mp3');
+  audio.play();
+}
+function bellx()
+{
+  var audio = new Audio('x.mp3');
+  audio.play();
+}
+function belly()
+{
+  var audio = new Audio('y.mp3');
+  audio.play();
+}
+function bellz()
+{
+  var audio = new Audio('z.mp3');
+  audio.play();
+}
+
